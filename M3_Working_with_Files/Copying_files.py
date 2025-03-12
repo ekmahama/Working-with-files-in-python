4770124abd028302e94e1ea0277e7e828a01b65d
Edward Mahama <mahamaek@gmail.com>
2025-03-12 16:17:52 -0400

    copy files

--- M3_Working_with_Files/Copying_files.py
+++ M3_Working_with_Files/Copying_files.py
@@ -6,7 +6,5 @@ def copy_files(src, dst):
 def copy_folders(src, dst):
     shutil.copytree(src, dst)
 
-
-
 #copy_files("./files/02_file.txt", "./files/subfolder")
 copy_folders("./files", "./files/new_flder")
