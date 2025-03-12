c7a6fd64d97559f8af20cecb42298ed7669d11b4
Edward Mahama <mahamaek@gmail.com>
2025-03-12 16:09:50 -0400

    travers a folder

---
+++ M3_Working_with_Files/Traversing_directory.py
@@ -0,0 +1,10 @@
+import os
+
+def traverse(fld):
+    for fldpath, dirs, fls in os.walk(fld):
+        print(f"Folder: {fldpath}")
+        for fn in fls:
+            print(f"\t{fn}")
+
+
+traverse("./files")
